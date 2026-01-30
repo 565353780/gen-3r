57fb8321aff3de6cdaf8375ef5c76ca6df035d49
chLi <565353780@qq.com>
2026-01-30 10:26:08 +0800

    create run command

---
+++ run.sh
@@ -0,0 +1,8 @@
+python infer.py \
+  --pretrained_model_name_or_path ${HOME}/chLi/Model/GEN3R/Gen3R \
+  --task allview \
+  --prompts prompt.txt \
+  --frame_path ${HOME}/chLi/Dataset/GS/haizei_1_v4/gs/images/ \
+  --cameras free \
+  --output_dir ${HOME}/chLi/Dataset/GS/haizei_1_v4/gen3r/ \
+  --remove_far_points
